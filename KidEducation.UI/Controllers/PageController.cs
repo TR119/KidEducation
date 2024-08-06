b17ef4bfdb8ea06d03c1c53ed84139845c9d09dc
tienlq119 <luuquangtien119@gmail.com>
2024-08-06 17:29:01 +0700

    cm

---
+++ KidEducation.UI/Controllers/PageController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KidEducation.UI.Controllers
+{
+    
+    public class PageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Services()
+        {
+            return View();
+        }
+        public IActionResult Pricing()
+        {
+            return View();
+        }
+
+        public IActionResult FAQ()
+        {
+            return View();
+        }
+
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
